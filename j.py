e2929619ceea34390efc777b3710d793bd4c339b
junelynpalma <74013446+junelynpalma@users.noreply.github.com>
2021-09-24 15:05:56 +0800

    Update j.py

--- j.py
+++ j.py
@@ -3,10 +3,10 @@ import time
 import os
 import sys
 
-os.system('node g.js http://dogcorp.co/LOGIN http.txt 600 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
+os.system('node g.js https://dogcorp.co/LOGIN http.txt 600 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
 
 def job():
-    os.system('node g.js http://dogcorp.co/LOGIN http.txt 600 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
+    os.system('node g.js https://dogcorp.co/LOGIN http.txt 600 GET PHPSESSID:ohq0tcrvhgfuo6ljh8mn20ndu7')
     
 schedule.every(1).seconds.do(job)
 
